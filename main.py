d41b411bdf1976a608bfe1f664e5f9296195524e
Aimen Sahnoun <62159014+aimensahnoun@users.noreply.github.com>
2022-05-18 13:05:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from PIL import Image
 from io import BytesIO
 from models.pyTess import pyTessMain
 from models.u2net import u2Main
+import base64
+from flask import jsonify
+import os
+import pytesseract
+import cv2
+
 
 from flask import Flask, request, send_file
 
@@ -12,25 +18,52 @@ app = Flask(__name__)
 def TextExtract():
     img = Image.open(r"./images/incImg/temp.png")
 
-    imgText = pyTessMain.tesseractExtract(img)
+    # imgText = pyTessMain.tesseractExtract(img)
 
     return imgText
 
 
-@app.route('/objectEx', methods=['GET', 'POST'])
+@app.route('/expoObjectEx', methods=['GET', 'POST'])
 def ObjectExtract():
     if request.method == 'GET':
-        print("GAY")
-        return "GAY"
+        return "You can start"
     else:
-        test = request.files.get('image','')
-        newImg = Image.open(test)
-        img = Image.open(r"./images/IMGin/obj4.png")
+        print("Image request ====================")
+        bytesOfImage = request.get_data()
+        with open('image.jpeg', 'wb') as out:
+            out.write(bytesOfImage)
+        img = Image.open("./image.jpeg")
 
-        result = u2Main.remove(newImg, model_name="u2net", alpha_matting=True)
+        result = u2Main.remove(img, model_name="u2net", alpha_matting=True)
         resultImg = Image.open(BytesIO(result)).convert("RGBA")
-        #resultImg.show()
-        resultImg.save(r"./images/IMGout/result2.png")
+        # resultImg.show()
 
-        print("PENIS ",send_file(resultImg, mimetype='image/*') )
-        return send_file(resultImg, mimetype='image/*')
+        base64Value = get_base64(resultImg)
+
+        return jsonify({"imgUri64": base64Value})
+
+
+@app.route('/expoTextEx', methods=['GET', 'POST'])
+def extract_text():
+    bytesOfImage = request.get_data()
+    with open('image.jpeg', 'wb') as out:
+        out.write(bytesOfImage)
+    im = cv2.imread("./image.jpeg")
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(imgray, 180, 255, cv2.THRESH_BINARY)
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, thresh1)
+    img = Image.open(filename)
+    text = pytesseract.image_to_string(img)
+    return text
+
+
+def get_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    return "data:image/png;base64," + img_str.decode()
+
+
+if __name__ == "__main__":
+    app.run()
