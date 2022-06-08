207b44d556c51b31311b140d86a19350827c0317
Aimen Sahnoun <62159014+aimensahnoun@users.noreply.github.com>
2022-06-08 22:57:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from io import BytesIO
+import io
 from models.pyTess import pyTessMain
 from models.u2net import u2Main
 import base64
@@ -21,6 +22,25 @@ def TextExtract():
     # imgText = pyTessMain.tesseractExtract(img)
 
     return imgText
+
+
+@app.route('/objectEx', methods=['GET', 'POST'])
+def NormalObjectExtract():
+    if request.method == 'GET':
+        return "You can start"
+    else:
+        print("Image request ====================")
+        imageBase64 = request.json["base64"]
+        img = Image.open(io.BytesIO(
+            base64.decodebytes(bytes(imageBase64, "utf-8"))))
+
+        result = u2Main.remove(img, model_name="u2net", alpha_matting=True)
+        resultImg = Image.open(BytesIO(result)).convert("RGBA")
+        # resultImg.show()
+
+        base64Value = get_base64(resultImg)
+
+        return jsonify({"imgUri64": base64Value})
 
 
 @app.route('/expoObjectEx', methods=['GET', 'POST'])
